631f7082e159fbd914e7c1d2b944919921939ff6
RomanRiqulme10 <105177309+RomanRiqulme10@users.noreply.github.com>
2023-11-13 22:11:51 -0300

    Update ClubController.php

--- tpe-especial-web2-ENTREGA-3/app/Controllers/ClubController.php
+++ tpe-especial-web2-ENTREGA-3/app/Controllers/ClubController.php
@@ -31,7 +31,7 @@ class ClubController extends ApiController{
       $club = $this->model->getCLubByID($params[':ID']);
 
       if(empty($club)){
-          $this->viewAPI->response( ['response' => 'Bad Request'],404);
+          $this->viewAPI->response( ['response' => 'Bad Request'],400);
       }else {
           $this->viewAPI->response($club, 200) ;
       }
