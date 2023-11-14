6e2c58e1f304cd47163634554656e0c6a148ff45
RomanRiqulme10 <105177309+RomanRiqulme10@users.noreply.github.com>
2023-11-13 22:21:10 -0300

    Update JugadorModel.php

--- tpe-especial-web2-ENTREGA-3/app/Models/JugadorModel.php
+++ tpe-especial-web2-ENTREGA-3/app/Models/JugadorModel.php
@@ -1,18 +1,6 @@
 <?php
 require_once './app/Models/Model.php' ;
 class JugadorModel extends Model {
- 
-        
-            
-            public function getJugador($id) {
-
-                $query = $this->db->prepare('SELECT * FROM jugadores WHERE id_jugador = ?');
-                $query->execute([$id]);
-
-                $jugador = $query->fetch(PDO::FETCH_OBJ);
-
-                return $jugador;
-            }
 
             public function getJugadores(){
 
