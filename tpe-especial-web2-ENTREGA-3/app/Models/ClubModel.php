808142dde370b28bb45255b6ec6a5449d5afba7e
RomanRiqulme10 <105177309+RomanRiqulme10@users.noreply.github.com>
2023-11-13 22:22:56 -0300

    Update ClubModel.php

--- tpe-especial-web2-ENTREGA-3/app/Models/ClubModel.php
+++ tpe-especial-web2-ENTREGA-3/app/Models/ClubModel.php
@@ -2,15 +2,6 @@
 require_once './app/Models/Model.php' ;
 class ClubModel extends Model {
 
-             public function getClub($id){
-                
-                $query = $this->db->prepare('SELECT * FROM  clubes WHERE club_id = ?');
-                $query->execute([$id]);
-                $club = $query->fetch(PDO::FETCH_OBJ);
-            
-                return $club ;
-                
-            }
    
             public function getCLubByID($id) {
 
